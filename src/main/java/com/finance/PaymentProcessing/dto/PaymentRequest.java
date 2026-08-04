2b6e3ebd7dee8d7376d5e1a73bd038506574f523
Rishabh Srivastava <rishabhdav76@gmail.com>
2026-08-04 14:16:23 +0000

    Add DTO, Exception and Services

---
+++ src/main/java/com/finance/PaymentProcessing/dto/PaymentRequest.java
@@ -0,0 +1,21 @@
+package com.finance.PaymentProcessing.dto;
+
+import com.finance.PaymentProcessing.model.PaymentType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record PaymentRequest(
+                @NotNull @DecimalMin(value = "0.01") @DecimalMax(value = "1000000.00") @Digits(integer = 7, fraction = 2) BigDecimal amount,
+                @NotBlank String currency,
+                @NotBlank String reference,
+                @NotNull UUID beneficiaryId,
+                @NotNull UUID sourceAccountId,
+                @NotNull UUID payerId,
+                @NotNull PaymentType paymentType,
+                String invoiceId) {
+}
