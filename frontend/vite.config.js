9b4ea978bd6a744f464ef3209aeb3a09c48c292e
Diksha7876 <diksharana7876@gmail.com>
2026-08-06 09:12:18 +0000

    fix: frontend deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
+    port: 8081,
+    strictPort: true,
     proxy: {
       "/api": {
         target: "http://localhost:8082",
